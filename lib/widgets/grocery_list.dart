1625b0e1d1920a177f3cb8f3a1be30c991b58904
arkteck <arkteck@gmail.com>
2024-07-16 10:16:19 -0400

    handle 'no data' case

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -37,6 +37,13 @@ class _GroceryListState extends State<GroceryList> {
       return;
     }
 
+    if (response.body == 'null') {
+      setState(() {
+        _isLoading = false;
+      });
+      return;
+    }
+
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
